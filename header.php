392b002ebab4eaccdab813d27c1157ccf9dcb744
curvc <curvc@curvcui-MacBookPro.local>
2026-02-11 19:27:44 +0900

    Fix header links - use absolute URLs

--- header.php
+++ header.php
@@ -5,21 +5,23 @@
  */
 
 // 설정 파일 경로 결정
-$basePath = dirname(__FILE__); // header.php가 있는 디렉토리
+$basePath = dirname(__FILE__);
 require_once $basePath . '/config.php';
 
-// 현재 스크립트 경로에서 루트에서 호출인지 게임 페이지에서 호출인지 확인
+// 현재 스크립트 경로에서 게임 페이지인지 확인
 $scriptPath = $_SERVER['PHP_SELF'];
-
-// games 폴더 내에 있으면 ../index.php, 루트에 있으면 index.php
 $isGamePage = (strpos($scriptPath, '/games/') !== false);
+
+// 절대 경로로 링크 설정
+$homeUrl = 'http://tomseol.pe.kr/';
+$blogUrl = 'http://tomseol.pe.kr/blog/';
 ?>
 <header>
     <div class="header-content">
-        <a href="<?= $isGamePage ? '../index.php' : 'index.php' ?>" class="logo">🎮 <?= SITE_NAME ?></a>
+        <a href="<?= $homeUrl ?>" class="logo">🎮 <?= SITE_NAME ?></a>
         <nav>
-            <a href="<?= $isGamePage ? '../index.php' : 'index.php' ?>" <?= !$isGamePage ? 'class="active"' : '' ?>>미니게임</a>
-            <a href="<?= $isGamePage ? '../blog/' : 'blog/' ?>">블로그</a>
+            <a href="<?= $homeUrl ?>" <?= !$isGamePage ? 'class="active"' : '' ?>>미니게임</a>
+            <a href="<?= $blogUrl ?>">블로그</a>
         </nav>
     </div>
 </header>
